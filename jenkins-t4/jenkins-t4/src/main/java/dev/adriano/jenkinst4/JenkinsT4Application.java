9cb6d8795bb5797dc2ee14f2dd7d77af924600e0
Adriano Viera dos Santos <adrianovs.info@gmail.com>
2021-11-23 14:51:04 -0300

    first commit

---
+++ jenkins-t4/jenkins-t4/src/main/java/dev/adriano/jenkinst4/JenkinsT4Application.java
@@ -0,0 +1,13 @@
+package dev.adriano.jenkinst4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsT4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsT4Application.class, args);
+	}
+
+}
---
+++ jenkins-t4/jenkins-t4/src/test/java/dev/adriano/jenkinst4/JenkinsT4ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.adriano.jenkinst4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsT4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
